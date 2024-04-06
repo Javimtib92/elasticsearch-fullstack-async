c7653b093f7485684cf5bd0477c68ea87f0ce262
Javier Muñoz Tous <javimtib92@gmail.com>
2024-04-06 17:42:16 +0200

    add search function to create mapping based on pydantic model

--- backend/app/search.py
+++ backend/app/search.py
@@ -1,6 +1,8 @@
-from typing import Optional
 import os
 
+from typing import Optional, List
+from datetime import datetime
+from pydantic import BaseModel
 from dotenv import load_dotenv
 from elasticsearch import AsyncElasticsearch
 
@@ -23,3 +25,34 @@ class Search:
 
 async def get_es() -> Optional[Search]:
     return Search()
+
+
+# Solution inspired by from https://dzone.com/articles/pydantic-and-elasticsearch-dynamic-couple
+
+type_map = {
+    str: "keyword",
+    datetime: "date",
+    int: "long",
+    float: "float",
+    list: "keyword",
+    dict: "nested",
+    List[BaseModel]: "nested",
+}
+
+
+def create_es_mapping(pydantic_model: BaseModel):
+    mapping = {}
+    for field, field_type in pydantic_model.__annotations__.items():
+        es_field_type = type_map.get(field_type)
+        if not es_field_type:
+            if issubclass(field_type, BaseModel):
+                es_field_type = create_es_mapping(field_type)
+            else:
+                # assuming List[BaseModel] for nested types
+                es_field_type = {
+                    "type": "nested",
+                    "properties": create_es_mapping(field_type.__args__[0]),
+                }
+        mapping[field] = {"type": es_field_type}
+    print(mapping)
+    return mapping
