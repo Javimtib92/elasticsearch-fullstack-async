00599ffb60176219c5d44634ba01f7a281976f6e
Javier Muñoz Tous <javimtib92@gmail.com>
2024-04-07 21:15:22 +0200

    add tanstack router

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
+	plugins: [react(), TanStackRouterVite()],
+	server: {
+		port: 3000,
+	},
 });
