f354d41fd8b2a87dd6a5ee749143f9c100a6857a
Javier Muñoz Tous <javimtib92@gmail.com>
2024-04-07 21:27:11 +0200

    add path alias to vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,17 @@
+import path from "node:path";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), TanStackRouterVite()],
-	server: {
-		port: 3000,
-	},
+  plugins: [react(), TanStackRouterVite()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
