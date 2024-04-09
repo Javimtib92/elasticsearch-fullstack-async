afd0c9aa471e52564cba408b80d9420fb96dcb67
Javier Muñoz Tous <javimtib92@gmail.com>
2024-04-09 12:04:02 +0200

    extract burger menu component

--- frontend/src/components/mobile-nav.tsx
+++ frontend/src/components/mobile-nav.tsx
@@ -5,8 +5,8 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { cn } from "@/lib/utils";
 import { Link, useRouterState } from "@tanstack/react-router";
 import { useState } from "react";
-import { Button } from "./ui/button";
-import { Logo } from "./logo";
+import { Logo } from "@/components/logo";
+import { BurgerMenu } from "@/components/burger-menu";
 
 const NAV_ITEMS = [
   {
@@ -35,42 +35,7 @@ export function MobileNav({ className, ...props }: { className?: string }) {
       <Logo className="w-24 text-stone-800" />
       <Sheet open={open} onOpenChange={setOpen}>
         <SheetTrigger asChild>
-          <Button
-            variant="ghost"
-            className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
-          >
-            <svg
-              strokeWidth="1.5"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-            >
-              <title>Menu</title>
-              <path
-                d="M3 5H11"
-                stroke="currentColor"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M3 12H16"
-                stroke="currentColor"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M3 19H21"
-                stroke="currentColor"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            <span className="sr-only">Toggle Menu</span>
-          </Button>
+          <BurgerMenu />
         </SheetTrigger>
         <SheetContent side="right" className="pr-0">
           <Link
@@ -78,7 +43,9 @@ export function MobileNav({ className, ...props }: { className?: string }) {
             className="flex items-center"
             onClick={() => setOpen(false)}
           >
-            <span className="font-bold">Newtral Tech Test</span>
+            <span className="font-bold">
+              <Logo className="w-24 text-stone-800" />
+            </span>
           </Link>
           <ScrollArea className="my-4 h-[calc(100vh-8rem)] pb-10 pl-6">
             <div className="flex flex-col space-y-3">
